1011faed6d5ba3fc80fb3d92f2131a2ce7e1f457
HuangJunYao <3185536403@qq.com>
2025-04-08 18:19:32 +0800

    项目初始化——测试1

---
+++ springboot-init-master/src/main/java/com/model/springbootinit/mapper/UserAnswerMapper.java
@@ -0,0 +1,18 @@
+package com.model.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import generator.domain.UserAnswer;
+
+/**
+* @author 86175
+* @description 针对表【user_answer(用户答题记录)】的数据库操作Mapper
+* @createDate 2025-04-08 18:14:32
+* @Entity generator.domain.UserAnswer
+*/
+public interface UserAnswerMapper extends BaseMapper<UserAnswer> {
+
+}
+
+
+
+
