1b6bf2d34f1b4d7f4d620683acb46635c357fe84
HuangJunYao <3185536403@qq.com>
2025-04-09 15:21:19 +0800

    项目初始化

---
+++ springboot-init-master/generator/service/ScoringResultService.java
@@ -0,0 +1,51 @@
+package com.model.springbootinit.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.model.springbootinit.model.dto.scoringResult.ScoringResultQueryRequest;
+import com.model.springbootinit.model.entity.ScoringResult;
+import com.model.springbootinit.model.vo.ScoringResultVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 问题服务
+ *
+ */
+public interface ScoringResultService extends IService<ScoringResult> {
+
+    /**
+     * 校验数据
+     *
+     * @param scoringResult
+     * @param add 对创建的数据进行校验
+     */
+    void validScoringResult(ScoringResult scoringResult, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param scoringResultQueryRequest
+     * @return
+     */
+    QueryWrapper<ScoringResult> getQueryWrapper(ScoringResultQueryRequest scoringResultQueryRequest);
+    
+    /**
+     * 获取问题封装
+     *
+     * @param scoringResult
+     * @param request
+     * @return
+     */
+    ScoringResultVO getScoringResultVO(ScoringResult scoringResult, HttpServletRequest request);
+
+    /**
+     * 分页获取问题封装
+     *
+     * @param scoringResultPage
+     * @param request
+     * @return
+     */
+    Page<ScoringResultVO> getScoringResultVOPage(Page<ScoringResult> scoringResultPage, HttpServletRequest request);
+}
---
+++ springboot-init-master/generator/service/UserAnswerService.java
@@ -0,0 +1,51 @@
+package com.model.springbootinit.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.model.springbootinit.model.dto.userAnswer.UserAnswerQueryRequest;
+import com.model.springbootinit.model.entity.UserAnswer;
+import com.model.springbootinit.model.vo.UserAnswerVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 问题服务
+ *
+ */
+public interface UserAnswerService extends IService<UserAnswer> {
+
+    /**
+     * 校验数据
+     *
+     * @param userAnswer
+     * @param add 对创建的数据进行校验
+     */
+    void validUserAnswer(UserAnswer userAnswer, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param userAnswerQueryRequest
+     * @return
+     */
+    QueryWrapper<UserAnswer> getQueryWrapper(UserAnswerQueryRequest userAnswerQueryRequest);
+    
+    /**
+     * 获取问题封装
+     *
+     * @param userAnswer
+     * @param request
+     * @return
+     */
+    UserAnswerVO getUserAnswerVO(UserAnswer userAnswer, HttpServletRequest request);
+
+    /**
+     * 分页获取问题封装
+     *
+     * @param userAnswerPage
+     * @param request
+     * @return
+     */
+    Page<UserAnswerVO> getUserAnswerVOPage(Page<UserAnswer> userAnswerPage, HttpServletRequest request);
+}
